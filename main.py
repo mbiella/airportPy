71acd2ad155e6cd90b825f03df7682bf4333b6e6
Your Name <you@example.com>
2022-01-17 15:53:04 +0100

    Ver. 1.0.0

--- main.py
+++ main.py
@@ -3,11 +3,30 @@ from database import *
 con = creaConn()
 cur = creaCursor(con)
 
-#executeSQL("INSERT INTO flyState VALUES(1, 'Boarding')", cur)
+executeSQL("INSERT INTO AirplanesData VALUES(1, 'AIRBUS', 'A320neo', 'I-MLBC', 1)", cur)
+executeSQL("INSERT INTO AirplanesData VALUES(2, 'AIRBUS', 'A320neo', 'I-MB01', 2)", cur)
 
+print("Dati Aerei -----------")
+for row in cur.execute('SELECT * from AirplanesData'):
+    print(row)
 
+print("Aeroporti -----------")
+for row in cur.execute('SELECT * from Airports'):
+    print(row)
+
+print("Compagnie -----------")
 for row in cur.execute('SELECT * from Company'):
     print(row)
+
+print("Voli -----------")
+for row in cur.execute('SELECT * from FlyDates'):
+    print(row)
+
+print("Stati -----------")
+for row in cur.execute('SELECT * from FlyState'):
+    print(row)
+
+
 
 con.commit()
 cur.close()
