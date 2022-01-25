921b14e41f34f714c0a16e84c56d6edc02331b03
Your Name <you@example.com>
2022-01-25 15:13:16 +0100

    Version 1.1.4

--- main.py
+++ main.py
@@ -5,33 +5,45 @@ cur = creaCursor(con)
 
 #executeSQL("INSERT INTO FlyDates VALUES(1, '20220120', '1255', 1, 3, 2, 'AA238', 2, 7, 'B', 'B', 12, 'Nessuna')", cur)
 
-print("Dati Aerei -----------")
-for row in cur.execute('SELECT * from AirplanesData'):
-    print(row)
+def printInfo():
+    print("Dati Aerei -----------")
+    for row in cur.execute('SELECT * from AirplanesData'):
+        print(row)
 
-print("Aeroporti -----------")
-for row in cur.execute('SELECT * from Airports'):
-    print(row)
+    print("Aeroporti -----------")
+    for row in cur.execute('SELECT * from Airports'):
+        print(row)
 
-print("Compagnie -----------")
-for row in cur.execute('SELECT * from Company'):
-    print(row)
+    print("Compagnie -----------")
+    for row in cur.execute('SELECT * from Company'):
+        print(row)
 
-print("Voli -----------")
-for row in cur.execute('SELECT * from FlyDates'):
-    print(row)
+    print("Voli -----------")
+    for row in cur.execute('SELECT * from FlyDates'):
+        print(row)
 
-print("Stati -----------")
-for row in cur.execute('SELECT * from FlyState'):
-    print(row)
+    print("Stati -----------")
+    for row in cur.execute('SELECT * from FlyState'):
+        print(row)
+    print("VOLO -----------")
+    for row in cur.execute('select F.flyDate as Date, F.flyTime as Time, A.airportName as Departure,A2.airportName as Destination, F.flyNumber as Fly_Number, P.airplaneModel,FS.flyState,flyCheckInArea,flyGateArea,flyGate, C.companyName from FlyDates as F join Airports as A on F.flyDepartureID = A.IDAirport join Airports as A2 on F.flyDestinationID = A2.IDAirport join AirplanesData as P on F.flyAirplaneID = P.IDAirplane join FlyState FS on F.flyStateID = IDFlyState join Company as C on P.IDCompany = C.IDCompany'):
+        print(row)
+ 
+printInfo()
+
+while(1):
+    res=input("Vuoi cambiare lo stato (y/n)?: ")
+    if(res=='y'):
+        fly=input("Numero del volo?: ")
+        st=input("Nuovo stato (1-9)?")
+        cur.execute("UPDATE flyDates set flyStateID=" + st + " WHERE flyNumber='" + fly + "'")
+        con.commit()
+        printInfo()
+    else:
+        print("Program is shutting down...")
+        exit()
 
 
-print("VOLO -----------")
-for row in cur.execute('select F.flyDate as Date, F.flyTime as Time, A.airportName as Departure,A2.airportName as Destination, F.flyNumber as Fly_Number, P.airplaneModel,FS.flyState,flyCheckInArea,flyGateArea,flyGate, C.companyName from FlyDates as F join Airports as A on F.flyDepartureID = A.IDAirport join Airports as A2 on F.flyDestinationID = A2.IDAirport join AirplanesData as P on F.flyAirplaneID = P.IDAirplane join FlyState FS on F.flyStateID = IDFlyState join Company as C on P.IDCompany = C.IDCompany where F.IDFlyData = 1'):
-    print(row) 
-
-
-con.commit()
 cur.close()
 con.close()
 
